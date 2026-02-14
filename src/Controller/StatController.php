60d2ad1aab97ce661cd8d0abff391a1b4361b7a1
mathias <211731037+Wendybest912@users.noreply.github.com>
2026-02-14 20:12:44 +0100

    commit done

--- src/Controller/StatController.php
+++ src/Controller/StatController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Game;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -31,21 +32,45 @@ class StatController extends AbstractController {
 
     }
 
-    #[Route('/historique', name: 'statistique_historique')]
-    public function historique(ManagerRegistry $doctrine): Response
+   #[Route('/historique', name: 'statistique_historique')]
+    public function historique(ManagerRegistry $doctrine, Request $request): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         $em = $doctrine->getManager();
         $gameRepo = $em->getRepository(Game::class);
 
-        $myGames = $gameRepo->findBy(
-            ['player' => $this->getUser()],
-            ['id' => 'DESC']
-        );
+        $tri = $request->query->get('tri', 'id');
+        $ordre = $request->query->get('ordre', 'DESC');
+        $filtreDifficulte = $request->query->get('difficulte', '');
+        $filtreResultat = $request->query->get('resultat', '');
+
+        $trisAutorises = ['id', 'difficulty', 'guessNumber', 'winOrLose'];
+        if (!in_array($tri, $trisAutorises)) {
+            $tri = 'id';
+        }
+        if (!in_array($ordre, ['ASC', 'DESC'])) {
+            $ordre = 'DESC';
+        }
+
+        // Construire les critères de recherche
+        $criteres = ['player' => $this->getUser()];
+
+        if ($filtreDifficulte !== '') {
+            $criteres['difficulty'] = $filtreDifficulte;
+        }
+        if ($filtreResultat !== '') {
+            $criteres['winOrLose'] = $filtreResultat;
+        }
+
+        $myGames = $gameRepo->findBy($criteres, [$tri => $ordre]);
 
         return $this->render('hangman/historique.html.twig', [
             'games' => $myGames,
+            'tri' => $tri,
+            'ordre' => $ordre,
+            'filtreDifficulte' => $filtreDifficulte,
+            'filtreResultat' => $filtreResultat,
         ]);
     }
 
